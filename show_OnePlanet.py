66806a8901d2480276bf392dce759355357c14a6
Michal Biros <michal@keeman.world>
2023-08-13 12:12:49 +0200

    show newreveals

--- show_OnePlanet.py
+++ show_OnePlanet.py
@@ -2,6 +2,9 @@
 import time
 import argparse
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import WebDriverException
 
 BASE_URL = "https://www.oneplanetnft.io/nfts/0x3c178321f5bc73494046a46b5a065f9211b7c65e/{}"
@@ -16,12 +19,15 @@ def show_nft(driver, nft_range, filter_newreveals=None):
     for i in nft_range:
         url = BASE_URL.format(i)
         try:
-            driver.get(url)        
+            driver.get(url)  
+            WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-btn-type="refresh"]'))
+            )      
         except WebDriverException as we:
             print(f"WebDriver error occurred on NFT {i}: {we}")
 
-        # Wait for 3 seconds before loading the next page
-        time.sleep(3)
+        # Wait for 5 seconds before loading the next page
+        time.sleep(5)
 
 def main(start_nft, end_nft, filter_newreveals=None):
     # Safari's driver comes built-in, no need to specify a path
@@ -32,7 +38,7 @@ def main(start_nft, end_nft, filter_newreveals=None):
 
     driver.quit()
 
-def run_one_planet(start_nft, end_nft, filter_newreveals=False):
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Show Arkadians on OnePlanet')
     parser.add_argument('start_nft', type=int, help='Start of the NFT range')
     parser.add_argument('end_nft', type=int, help='End of the NFT range')
